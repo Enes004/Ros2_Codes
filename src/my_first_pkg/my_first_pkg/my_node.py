eb13e283fc5ebb4b0dc653f19f071c7342a3b36e
Enes004 <enessatici20@gmail.com>
2025-12-03 21:16:31 +0300

    first ros2 commit

---
+++ src/my_first_pkg/my_first_pkg/my_node.py
@@ -0,0 +1,43 @@
+import rclpy
+from rclpy.node import Node
+
+# MyNode sınıfı, ROS2'nin "Node" sınıfından miras (inheritance) alır.
+# Yani: "Sen artık sıradan bir Python sınıfı değil, bir ROS Robot Düğümüsün" diyoruz.
+class MyNode(Node):
+
+    def __init__(self):
+        # 1. Önce miras aldığımız Node sınıfını başlatıyoruz.
+        # 'ilk_dugum' -> Bu düğümün ROS ağındaki adı (kimliği) olacak.
+        super().__init__('ilk_dugum') 
+
+        # 2. Terminale log (mesaj) basmak için print() yerine bunu kullanırız.
+        # Çünkü bu loglar ROS sistemine tarih/saat etiketiyle kaydedilir.
+        self.get_logger().info('ROS2 Dünyasına Hoş Geldin Enes!')
+
+        # 3. Bir zamanlayıcı (Timer) kuruyoruz.
+        # 1.0 -> Kaç saniyede bir çalışsın? (1.0 saniye)
+        # self.timer_callback -> Süre dolunca hangi fonksiyonu çalıştırsın?
+        self.create_timer(1.0, self.timer_callback)
+
+    # Zamanlayıcı her tetiklendiğinde burası çalışır (Sözel döngü gibi düşün)
+    def timer_callback(self):
+        self.get_logger().info('Merhaba! Kodun başarıyla çalışıyor.')
+
+def main(args=None):
+    # 1. ROS2 iletişim sistemini başlatır (Motoru çalıştır).
+    rclpy.init(args=args)
+
+    # 2. Yukarıda yazdığımız sınıftan bir nesne (robot beyni) oluşturuyoruz.
+    node = MyNode()
+
+    # 3. BURASI ÇOK ÖNEMLİ: rclpy.spin()
+    # Bu komut "Programı kapatma, açık tut ve gelecek olayları (timer, sensör verisi vs.) bekle" der.
+    # Eğer bunu yazmazsan program "node = MyNode()" satırından sonra hemen kapanır.
+    # Sonsuz döngü (while True) gibi çalışır ama işlemciyi yormaz.
+    rclpy.spin(node)
+
+    # 4. Program kapatılınca (Ctrl+C yapılınca) temizlik yapar ve ROS'u kapatır.
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ src/my_first_pkg/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_first_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='enes',
+    maintainer_email='enessatici20@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            # 'komut_adi = paket_adi.dosya_adi:fonksiyon_adi'
+            'baslat = my_first_pkg.my_node:main',
+        ],
+    },
+)
